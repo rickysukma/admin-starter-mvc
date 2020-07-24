a51f7046daf3af922a84d3ee777eb53530257a96
Ricky Sukma <rickysukma9a@gmail.com>
2020-07-24 09:32:59 +0700

    menu js

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,3 @@
-function loaddatas(page=null){
-    if(page !== null)page = page;
-    param = 'proses=loaddata&page='+page;
-    post_response_text('master_menu_slave.php',param,respon);
-    function respon() {
-        if (con.readyState == 4) {
-            if (con.status == 200) {
-                if (!isSaveResponse(con.responseText)) {
-                    alert(con.responseText);
-                } else {
-                    // Success Response
-                    document.getElementById('isihalaman') = con.responseText;
-                    // updBlokDropdown();
-                }
-            } else {
-                error_catch(con.status);
-            }
-        }
-    }
-}
-
 loaddata(1);
 
 function loaddata(page){
